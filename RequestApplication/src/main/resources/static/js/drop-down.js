bba3ff283a3f5f849b73b1e7ced465da22c093b3
tamurayh <64111143+tamurayh@users.noreply.github.com>
2021-12-20 21:45:16 +0900

    プルダウン追加js

---
+++ RequestApplication/src/main/resources/static/js/drop-down.js
@@ -0,0 +1,37 @@
+$(function() {
+  var prefOption = '',
+    value,
+    prefdata,
+    prefLength = pref.length;
+  $('#Forst_Choice_Ctiy').hide();
+  for (var i = 0; i < prefLength; i++) {
+    prefdata = pref[i]
+    value = prefdata[0];
+    if (value == '都道府県を選択してください') {
+      prefOption += '<option value="" selected>' + value + '</option>\n';
+    } else {
+      prefOption += '<option value="' + value + '">' + value + '</option>\n';
+    }
+    $('#Forst_Choice_Pretecture').html(prefOption);
+  }
+
+  $('#Forst_Choice_Pretecture').on('change', function() {
+    var index = $(this).prop('selectedIndex');
+    var selected = $(this).val();
+    if (selected !== '') {
+      var cityOption = '';
+      $.each(pref[index][1], function() {
+        if (this == '市区町村で絞り込む') {
+          cityOption += '<option value="">' + this + '</option>\n';
+          console.log(this);
+        } else {
+          cityOption += '<option value="' + this + '">' + this + '</option>\n';
+          console.log(this);
+        }
+      });
+      $('#Forst_Choice_Ctiy').html(cityOption).fadeIn();
+    } else {
+      $('#Forst_Choice_Ctiy').html('<option value="">市区町村で絞り込む</option>').fadeOut();
+    }
+  });
+});
