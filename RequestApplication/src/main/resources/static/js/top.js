df481cfabf14d72294185e4f7d4d5b1f412116b3
tamurayh <64111143+tamurayh@users.noreply.github.com>
2021-12-30 12:48:29 +0900

    TOPの住みたいランキングをfadein化

--- RequestApplication/src/main/resources/static/js/top.js
+++ RequestApplication/src/main/resources/static/js/top.js
@@ -1,6 +1,7 @@
 $(function() {
 	
 	$('#sample').hide();/* 「#sample」を隠す */
+	$('.yokohamabox').hide();/* 「.yokohamabox」を隠す */
 
 	$(window).scroll(function()/* windowがスクロールされたら以下の処理を実行する */ {
 		$('#sample').each(function()/* .each()で、「#sample」が付いた要素1つずつに順番に処理 */{
@@ -23,7 +24,34 @@ $(function() {
 			
 			$('#sample').fadeout(1500);
 				
+			
 			}
+			
 		 });
-	 });
+	$('.yokohamabox').each(function()/* .each()で、「#sample」が付いた要素1つずつに順番に処理 */{
+			
+		/* .scrollTop()で、スクロールした量を取得して、変数scrollに代入 */
+		var scroll = $(window).scrollTop();
+		
+		/* .offset().topで、ページの最上部から「#sample」が付いた要素までの距離を取得して、変数positionに代入 */
+		var position =  $(this).offset().top;
+		
+		/* .height()で、ウィンドウの高さを取得して、変数windowHeightに代入 */
+		var windowHeight = $(window).height();
+		
+		/* スクロール量が、「ページ最上部から要素までの距離」-「ウィンドウの高さ」を超えた時 */
+		if (scroll > position - windowHeight + 1000){
+			
+			$('.yokohamabox').fadeIn(3000);/* 「#sample」を徐々に表示 */
+			
+			}else{
+			
+			$('.yokohamabox').fadeout(1500);
+				
+			
+			}
+		
+			
+		 });
+ 	});
  });
