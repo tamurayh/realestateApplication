bbb0776aa28e9c92c7dd880544cd45eb9fb35677
tamurayh <64111143+tamurayh@users.noreply.github.com>
2021-12-31 14:26:59 +0900

    第二条件 市町村プルダウン追加、fadeIn追加

--- RequestApplication/src/main/resources/static/js/drop-down.js
+++ RequestApplication/src/main/resources/static/js/drop-down.js
@@ -10,6 +10,7 @@ $(function() {
 	$('#Forst_Choice_Ctiy_item').hide();/* 「#Forst_Choice_Ctiy_item」を隠す */
 	$('#Second_Choice_Pretecture_item').hide();
  	$('#Forst_Choice_Ctiy').hide();
+	$('#Second_Choice_Ctiy_item').hide();
   for (var i = 0; i < prefLength; i++) {
     prefdata = pref[i]
     value = prefdata[0];
@@ -43,7 +44,6 @@ $(function() {
     }
   });
 $('#Forst_Choice_Ctiy').on('change', function() {
-	var index = $(this).prop('selectedIndex');
     var selected = $(this).val()
 	if (selected !== '') {
 	for (var i = 0; i < SecondprefLength; i++) {
@@ -62,4 +62,25 @@ $('#Forst_Choice_Ctiy').on('change', function() {
 	
 }
 });
+  $('#Second_Choice_Pretecture').on('change', function() {
+    var index = $(this).prop('selectedIndex');
+    var selected = $(this).val();
+    if (selected !== '') {
+      var cityOption = '';
+      $.each(pref[index][1], function() {
+        if (this == '市区町村で絞り込む') {
+          cityOption += '<option value="">' + this + '</option>\n';
+          console.log(this);
+        } else {
+          cityOption += '<option value="' + this + '">' + this + '</option>\n';
+          console.log(this);
+        }
+      });
+      $('#Second_Choice_Ctiy').html(cityOption).fadeIn();
+	　$('#Second_Choice_Ctiy_item').fadeIn();
+    } else {
+      $('#Second_Choice_Ctiy').html('<option value="">市区町村で絞り込む</option>').fadeOut();
+	　$('#Second_Choice_Ctiy_item').fadeOut()
+    }
+  });
 });
