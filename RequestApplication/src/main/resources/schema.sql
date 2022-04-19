8155ccd4020eebf5aef29a7ff507b00c387a9833
tamurayh <64111143+tamurayh@users.noreply.github.com>
2022-04-19 17:08:39 +0900

    test

--- RequestApplication/src/main/resources/schema.sql
+++ RequestApplication/src/main/resources/schema.sql
@@ -7,24 +7,3 @@ CREATE TABLE test_table
    old INT,
    PRIMARY KEY(id)
 );
-CREATE TABLE　condition1
-(
-   No INT NOT NULL AUTO_INCREMENT,
-   Forst_Choice_Pretecture VARCHAR(5) NOT NULL,
-   Forst_Choice_Ctiy VARCHAR(5) NOT NULL,
-   Second_Choice_Pretecture VARCHAR(5),
-   Second_Choice_Ctiy VARCHAR(5),
-   Third_Choice_Pretecture VARCHAR(5),
-   Third_Choice_Ctiy VARCHAR(5),
-   Asking_price_min varchar(5).
-   Asking_price_max varchar(5).
-   Floop_Plon_num varchar(5).
-   Floop_Plon varchar(5) NOT NULL.
-   Terms_and_Conditions1 varchar(10). 
-   Terms_and_Conditions2 varchar(10).
-   Terms_and_Conditions3 varchar(10).
-   Terms_and_Conditions4 varchar(10).
-   Terms_and_Conditions5 varchar(10).
-   userid bigint
-   PRIMARY KEY(id)
-);
